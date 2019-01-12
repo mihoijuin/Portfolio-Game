9c28e4c0caa8190e01eb8207099e21a7447f3dba
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-01-12 21:14:36 +0900

    [add] refs #21 シーン訳けをなくして、画面状態を更新する仕様に変更（テストケースも作成)

---
+++ Portfolio Game/Assets/Scripts/Tests/PlayMode/ScreenController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScreenController : MonoBehaviour {
+
+    public enum SCREEN
+    {
+        NONE = -1,
+        EXPLORE = 0,
+        CHARACTOR,
+        NUM
+    }
+
+    public SCREEN ScreenState { get; set; } = SCREEN.NONE;
+
+
+    private void Start()
+    {
+        ScreenState = SCREEN.EXPLORE;
+    }
+
+    public void GoToExprore()
+    {
+
+        // キャラクター画面のときのみ遷移できる
+        if(ScreenState == SCREEN.CHARACTOR)
+        {
+            ScreenState = SCREEN.EXPLORE;
+            Debug.Log("探索へ");
+        }
+    }
+
+    public void GoToCharactor()
+    {
+        // 探索画面のときのみ遷移できる
+        if(ScreenState == SCREEN.EXPLORE)
+        {
+            ScreenState = SCREEN.CHARACTOR;
+            Debug.Log("キャラクターへ");
+        }
+    }
+
+}
---
+++ Portfolio Game/Assets/Scripts/Tests/PlayMode/TestScreenController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestScreenController {
+
+    private class GoToExprore_TestScenario : ScreenController, IMonoBehaviourTest
+    {
+        public bool IsTestFinished { get; private set; }
+
+        private void Start()
+        {
+            StartCoroutine(TestScenario());
+        }
+
+        private IEnumerator TestScenario()
+        {
+
+            // キャラクターシーンからの遷移の状態
+            ScreenState = SCREEN.CHARACTOR;
+            // 1フレーム待機
+            yield return null;
+
+            GoToExprore();
+            // EXPLOREのインデックスは0
+            Assert.That((int)ScreenState, Is.EqualTo(0));
+
+            IsTestFinished = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private class GoToCharactor_TestScenario : ScreenController, IMonoBehaviourTest
+    {
+        public bool IsTestFinished { get; private set; }
+
+        private void Start()
+        {
+            StartCoroutine(TestScenario());
+        }
+
+        private IEnumerator TestScenario()
+        {
+            ScreenState = SCREEN.EXPLORE;
+            // 1フレーム待機
+            yield return null;
+
+            GoToCharactor();
+            // CHARACTORのインデックスは1
+            Assert.That((int)ScreenState, Is.EqualTo(1));
+
+            IsTestFinished = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    /// <summary>
+    /// シーン切り替えテスト
+    /// </summary>
+    [UnityTest]
+    public IEnumerator SwitchExploreScreen()
+    {
+        yield return new MonoBehaviourTest<GoToExprore_TestScenario>();
+    }
+
+    [UnityTest]
+    public IEnumerator SwitchCharactorScreen()
+    {
+        yield return new MonoBehaviourTest<GoToCharactor_TestScenario>();
+    }
+
+
+
+}
