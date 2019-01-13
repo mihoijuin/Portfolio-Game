bd8441c58d3aeb5918756b977acb98791876dea1
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-01-13 12:49:53 +0900

    [add]refs #12 アイテムとの当たり判定を実装

---
+++ Portfolio Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+    void Start () {
+
+
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            Debug.Log("item");
+        }
+    }
+
+}
