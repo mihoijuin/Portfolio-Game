3d22c44fcedac165dd231cc8381a6a6cdca8e4c9
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-01-16 10:10:34 +0900

    [add]refs #13 Playerの移動を実装（回転しないようにConstrainsで対処済み）

--- Portfolio Game/Assets/Scripts/PlayerController.cs
+++ Portfolio Game/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-
+    Rigidbody2D playerRigid;
+    public float playerSpeed;
 
     void Start () {
-
+        playerRigid = GetComponent<Rigidbody2D>();
 
 	}
-	
-	
-	void Update () {
-		
-	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveUp();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveDown();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+    }
 
 
+    void MoveUp()
+    {
+        playerRigid.MovePosition(playerRigid.position + Vector2.up * playerSpeed);
+    }
+
+    void MoveDown()
+    {
+        playerRigid.MovePosition(playerRigid.position + Vector2.down * playerSpeed);
+    }
+
+
+    void MoveRight()
+    {
+        playerRigid.MovePosition(playerRigid.position + Vector2.right * playerSpeed);
+    }
+
+    void MoveLeft()
+    {
+        playerRigid.MovePosition(playerRigid.position + Vector2.left * playerSpeed);
+    }
 }
