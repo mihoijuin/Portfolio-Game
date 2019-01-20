e9d21d095d59b7fc05985dadd49f780f108c2973
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-01-20 18:48:47 +0900

    [add]refs #29 キャラ画面へ切り替える動きを実装（ブラックアウト→上下から視界が晴れる）

--- Portfolio Game/Assets/Scripts/ScreenController.cs
+++ Portfolio Game/Assets/Scripts/ScreenController.cs
@@ -31,10 +31,19 @@ public class ScreenController : MonoBehaviour {
     public float zoomSpeed;
     public float zoomSize;
 
+    public float scaleSpeed;
+    public float scaleInterval;
+
+    // カメラ
+    Vector3 cameraOriginPos;
+    float cameraOriginSize;
+
 
     private void Start()
     {
         ScreenState = SCREEN.EXPLORE;
+        cameraOriginPos = Camera.main.transform.position;
+        cameraOriginSize = Camera.main.orthographicSize;
     }
 
     public bool IsExplore(){ return ScreenState == SCREEN.EXPLORE; }
@@ -84,10 +93,10 @@ public class ScreenController : MonoBehaviour {
         while (Camera.main.orthographicSize > zoomSize + 0.01f)
         {
             // カメラをドアの位置に移動
-            float targetPosx = Mathf.SmoothStep(Camera.main.transform.position.x, door.transform.position.x, zoomSpeed);
-            float targetPosy = Mathf.SmoothStep(Camera.main.transform.position.y, door.transform.position.y, zoomSpeed);
+            float targetPosX = Mathf.SmoothStep(Camera.main.transform.position.x, door.transform.position.x, zoomSpeed);
+            float targetPosY = Mathf.SmoothStep(Camera.main.transform.position.y, door.transform.position.y, zoomSpeed);
 
-            Camera.main.transform.position = new Vector3(targetPosx, targetPosy, -10f);
+            Camera.main.transform.position = new Vector3(targetPosX, targetPosY, cameraOriginPos.z);
 
             // カメラをズームイン
             Camera.main.orthographicSize = Mathf.SmoothStep(Camera.main.orthographicSize, zoomSize, zoomSpeed);
@@ -100,8 +109,27 @@ public class ScreenController : MonoBehaviour {
         player.GetComponent<Rigidbody2D>().isKinematic = true;
         player.transform.Translate(1.5f, 0, 0);
 
-        yield return new WaitForSeconds(0.8f);
+        // 画面切り替え用のオーバーレイを出現
+        yield return new WaitForSeconds(0.5f);
         overlay.SetActive(true);
+
+        // カメラを元に戻す
+        Camera.main.transform.position = cameraOriginPos;
+        Camera.main.orthographicSize = cameraOriginSize;
+
+        // 画面を切り替え
+        exploreScreen.SetActive(false);
+        charactorScreen.SetActive(true);
+
+        // オーバレイを消す
+        yield return new WaitForSeconds(1f);
+        while(overlay.transform.localScale.y > 0f)
+        {
+            float targetScaleY = Mathf.SmoothStep(overlay.transform.localScale.y, 0f, scaleSpeed);
+            overlay.transform.localScale = new Vector3(overlay.transform.localScale.x, targetScaleY, overlay.transform.localScale.z);
+            yield return new WaitForSeconds(scaleInterval);
+        }
+
 
     }
 
