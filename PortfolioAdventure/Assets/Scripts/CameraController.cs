a9c1e6726c248376bd8b635d54f5f516b8cfca6d
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-02-20 18:00:58 +0900

    [add]refs #34 Unityセットアップ、カメラとCanvasを複数解像度対応

---
+++ PortfolioAdventure/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private void Awake(){
+        // 開発時の縦横比
+        float developAspect = 1024f / 750.0f;
+        // 実機のサイズを取得して、縦横比取得
+        float deviceAspect = (float)Screen.width / (float)Screen.height;
+
+        // 実機と開発画面との対比
+        float scale = deviceAspect / developAspect;
+
+        Camera mainCamera = Camera.main;
+
+        // カメラに設定していたorthographicSizeを実機との対比でスケール
+        float deviceSize = mainCamera.orthographicSize;
+
+        // scaleの逆数
+        float deviceScale = 1.0f / scale;
+        // orthographicSizeを計算し直す
+        mainCamera.orthographicSize = deviceSize * deviceScale;
+
+    }
+}
