de98918b384dd8fc281d52b089f37a070352edcf
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-02-26 08:23:57 +0900

    [add]refs #35 デバッグメニューをシーン開始時に表示

---
+++ PortfolioAdventure/Assets/Scripts/SceneBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBase : MonoBehaviour
+{
+
+    public enum SCENE {
+        NONE = -1,
+
+        探索 = 0,
+        PORINのいる場所,
+
+        NUM
+    }
+
+    public enum SCENARIO
+    {
+        NONE = -1,
+
+        Chapter1_1 = 0,
+        Chapter1_2,
+        Chapter1_3,
+
+        NUM
+    }
+
+    [SerializeField]
+    GameObject debugCanvas;
+
+    private bool isInitialized = false;
+
+    public static SCENARIO currentScinario{ get; set; }
+
+    protected virtual void Awake(){
+        if(!isInitialized){
+            GameObject debugMenu = Instantiate(debugCanvas) as GameObject;
+            DontDestroyOnLoad(debugMenu);
+            isInitialized = true;
+        }
+    }
+}
---
+++ PortfolioAdventure/Assets/Scripts/SceneInvestigation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneInvestigation : SceneBase
+{
+
+    protected override void Awake(){
+        base.Awake();
+        Debug.Log(currentScinario);
+    }
+
+}
