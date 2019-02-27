130d9fe47f695229c404e3ebec4d2e7a49a10955
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-02-27 13:12:04 +0900

    [update]refs #35 デバッグ機能の追加しやすさを拡張

--- PortfolioAdventure/Assets/Scripts/DebugMenu.cs
+++ PortfolioAdventure/Assets/Scripts/DebugMenu.cs
@@ -6,64 +6,49 @@ using UnityEngine.UI;
 
 public class DebugMenu : MonoBehaviour
 {
-    //The list of messages for the Dropdown
-    List<Dropdown.OptionData>[] m_MessagesArray = new List<Dropdown.OptionData>[2];
-
     Dropdown[] m_Dropdowns;
     Button confirmButton;
 
     private void Awake(){
         m_Dropdowns = FindObjectsOfType<Dropdown>();
+        Array.Reverse(m_Dropdowns); // 表示順になるように
         confirmButton = transform.Find("Scroll View/Viewport/Content/ConfirmButton").GetComponent<Button>();
     }
 
     private void Start()
     {
+        List<Dropdown.OptionData>[] m_OptionDataListArray = new List<Dropdown.OptionData>[m_Dropdowns.Length];
+
         // ドロップダウン
-        Dropdown.OptionData[] m_NewDataArray = new Dropdown.OptionData[(int)SceneBase.SCENARIO.NUM];  // シナリオ選択
-        m_MessagesArray[0] = new List<Dropdown.OptionData>();
-        int scenarioIndex = 0;
-        foreach(string sceneName in  Enum.GetNames(typeof(SceneBase.SCENARIO))){
-            if(sceneName != "NONE" && sceneName != "NUM"){
-                m_NewDataArray[scenarioIndex] = new Dropdown.OptionData();
-                m_NewDataArray[scenarioIndex].text = sceneName;
-                m_MessagesArray[0].Add(m_NewDataArray[scenarioIndex]);
-                scenarioIndex += 1;
+        Type[] choiceArray = new Type[] {typeof(SceneBase.SCENE), typeof(SceneBase.SCENARIO)};   // ドロップダウンで選択するもの
+        foreach(Type choice in choiceArray){
+            string[] nameArray = choice.GetEnumNames();
+            Dropdown.OptionData[] m_NewDataArray = new Dropdown.OptionData[nameArray.Length-2];     // NONE, NUMを数えない
+            m_OptionDataListArray[Array.IndexOf(choiceArray, choice)] = new List<Dropdown.OptionData>();
+            foreach(string name in  nameArray){
+                if(name != "NONE" && name != "NUM"){
+                    m_NewDataArray[Array.IndexOf(nameArray, name)] = new Dropdown.OptionData();
+                    m_NewDataArray[Array.IndexOf(nameArray, name)].text = name;
+                    m_OptionDataListArray[Array.IndexOf(choiceArray, choice)].Add(m_NewDataArray[Array.IndexOf(nameArray, name)]);
+                }
             }
         }
 
-        m_NewDataArray = new Dropdown.OptionData[(int)SceneBase.SCENE.NUM];   // シーン選択
-        m_MessagesArray[1] = new List<Dropdown.OptionData>();
-        int sceneIndex = 0;
-        foreach(string sceneName in  Enum.GetNames(typeof(SceneBase.SCENE))){
-            if(sceneName != "NONE" && sceneName != "NUM"){
-                m_NewDataArray[sceneIndex] = new Dropdown.OptionData();
-                m_NewDataArray[sceneIndex].text = sceneName;
-                m_MessagesArray[1].Add(m_NewDataArray[sceneIndex]);
-                sceneIndex += 1;
-            }
-        }
-
-
-        for(int index=0; index<m_MessagesArray.Length; ++index){    // ドロップダウンに内容を入れていく
+        for(int index=0; index<m_OptionDataListArray.Length; ++index){    // ドロップダウンに内容を入れていく
             m_Dropdowns[index].ClearOptions();
-            foreach(Dropdown.OptionData message in m_MessagesArray[index]){
+            foreach(Dropdown.OptionData message in m_OptionDataListArray[index]){
                 m_Dropdowns[index].options.Add(message);
             }
         }
 
         // 確認ボタン
-        m_Dropdowns[1].captionText.text = Enum.GetNames(typeof(SceneBase.SCENE))[0];    // 実操作時はNullになるのでデフォルト値を設定
-        m_Dropdowns[0].captionText.text = Enum.GetNames(typeof(SceneBase.SCENARIO))[0];
-        UpdateConfirmButton();
-
-        int dropIndex = 0;
         foreach(Dropdown m_Dropdown in m_Dropdowns){
-            m_Dropdowns[dropIndex].onValueChanged.AddListener(delegate {
+            m_Dropdown.captionText.text = Enum.GetNames(choiceArray[Array.IndexOf(m_Dropdowns, m_Dropdown)])[0];    // 初期起動時はNullになるのでデフォルト値を設定
+            m_Dropdowns[Array.IndexOf(m_Dropdowns, m_Dropdown)].onValueChanged.AddListener(delegate {
                 UpdateConfirmButton();
             });
-            dropIndex += 1;
         }
+        UpdateConfirmButton();  // 初期起動時用
 
         // ゲーム開始時は操作不可に
         transform.Find("Scroll View/").gameObject.SetActive(false);
@@ -71,8 +56,9 @@ public class DebugMenu : MonoBehaviour
 
     private void UpdateConfirmButton(){
         confirmButton.onClick.RemoveAllListeners();
-        confirmButton.onClick.AddListener(() => SceneBase.LoadScenario(m_Dropdowns[1].captionText.text, m_Dropdowns[0].captionText.text));
-        confirmButton.onClick.AddListener(() => Debug.Log("[Debug Menu]シーン：" + m_Dropdowns[1].captionText.text + "、シナリオ：" + m_Dropdowns[0].captionText.text));
+        confirmButton.onClick.AddListener(() => SceneBase.LoadScenario(m_Dropdowns[0].captionText.text, m_Dropdowns[1].captionText.text));
+        confirmButton.onClick.AddListener(() => Debug.Log("[Debug Menu]シーン：" + m_Dropdowns[0].captionText.text + "、シナリオ：" + m_Dropdowns[1].captionText.text));
         confirmButton.onClick.AddListener(() => transform.Find("Scroll View/").gameObject.SetActive(false));
     }
+
 }
