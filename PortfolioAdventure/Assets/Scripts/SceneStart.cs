71cca08435605738072ec5763b87615441e7d223
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-02-27 15:05:11 +0900

    [add]refs #36 シナリオデータがないときはつづきからボタンを表示しない

---
+++ PortfolioAdventure/Assets/Scripts/SceneStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneStart : SceneBase
+{
+    protected override void Awake(){
+        base.Awake();
+        if(SceneBase.currentScinario == SCENARIO.NONE){
+            GameObject.Find("ContinueButton").SetActive(false);
+        }
+    }
+}
